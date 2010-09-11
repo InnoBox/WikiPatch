f0514eb417afa0eda8bea37a13b2cf69b5e7c48c
Benjamin M. Schwartz <bens@alum.mit.edu>
2010-09-11 12:27:42 -0400

    Give wiki_user DROP privileges on wiki_db, as is required whenever the database is replaced wholesale.

--- overlay/usr/share/innobox-wiki/prepareWikiUser.sql
+++ overlay/usr/share/innobox-wiki/prepareWikiUser.sql
@@ -2,5 +2,5 @@
 # The password field is set to a placeholder that is easily substituted
 # using python's string processing functions.
 create database wiki_db;
-grant index, create, select, insert, update, delete, alter, lock tables on wiki_db.* to 'wiki_user'@'localhost' identified by '%(WIKI_PASSWORD)s';
+grant drop, index, create, select, insert, update, delete, alter, lock tables on wiki_db.* to 'wiki_user'@'localhost' identified by '%(WIKI_PASSWORD)s';
 
