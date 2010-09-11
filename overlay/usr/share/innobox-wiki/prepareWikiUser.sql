7d0fceac44436f81acf152875443a63da576e8d7
Benjamin M. Schwartz <bens@alum.mit.edu>
2010-09-11 00:17:10 -0400

    Modify first-boot script to create the wiki_db database and wiki_user user, with appropriate password.

---
+++ overlay/usr/share/innobox-wiki/prepareWikiUser.sql
@@ -0,0 +1,6 @@
+# Copied from http://www.mediawiki.org/wiki/Manual:Installing_MediaWiki#MySQL
+# The password field is set to a placeholder that is easily substituted
+# using python's string processing functions.
+create database wiki_db;
+grant index, create, select, insert, update, delete, alter, lock tables on wiki_db.* to 'wiki_user'@'localhost' identified by '%(WIKI_PASSWORD)s';
+
